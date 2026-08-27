3de121a5968406de0fc2df65d2e777b5b6a775f9
Carol Xu <xucarol.20@gmail.com>
2026-08-27 14:34:25 -0400

    ⸜( ´ ꒳ ` )⸝

--- analysis/CPS_analysis.R
+++ analysis/CPS_analysis.R
@@ -67,16 +67,6 @@ cpsstates_2025 = cps %>%
     n = n(),
     population = sum(asecwt, na.rm = TRUE)) %>% ungroup()
 
-state_lookup = tibble(
-  statefip = c( 1,  2,  4,  5,  6,  8,  9, 10, 11, 12, 13, 15, 16,
-               17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-               30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,
-               44, 45, 46, 47, 48, 49, 50, 51, 53, 54, 55, 56, 72),
-  state_abb = c("AL","AK","AZ","AR","CA","CO","CT","DE","DC","FL","GA","HI","ID",
-                "IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO",
-                "MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA",
-                "RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY","PR"))
-
 cpsstates_2025 = cpsstates_2025 %>%
   left_join(state_lookup, by = "statefip")
 
